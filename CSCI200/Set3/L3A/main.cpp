265369da8e7f66856fc7ed4921c30563f6318e6d
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-02-26 05:38:44 -0700

    Wrote first iteration of code

--- CSCI200/Set3/L3A/main.cpp
+++ CSCI200/Set3/L3A/main.cpp
@@ -2,22 +2,52 @@
 #include <fstream>
 using namespace std;
 
-int main() {
-    ifstream myCatsAgesIn("filename"); // declare ifstream object and open the file
-
-    // check for an error
-    if ( myCatsAgesIn.fail() ) {
+int main()
+{
+    // Input file
+    ifstream secretMessage("secretMessage.txt");
+    if(secretMessage.fail())
+    {
         cerr << "Error opening input file";
         return -1;
     }
+    cout << "secretMessage.txt opened" << endl;
 
-    // read the data and do something with it
-    int age;
-    while( !myCatsAgesIn.eof() ) {
-        myCatsAgesIn >> age;
-        cout << "One cat is " << age << " years old.\n";
+    // Output file
+    ofstream decipheredMessage("decipheredMessage.txt");
+    if(decipheredMessage.fail())
+    {
+        cerr << "Error opening output file";
+        return -1;
+    }
+    cout << "decipheredMessage.txt created" << endl;
+
+    // Decipher message
+    char currentChar;
+    while(secretMessage.eof())
+    {
+        secretMessage >> currentChar;
+        cout << "Reading current character: " << currentChar << endl;
+
+        // Process current character and output to decipheredMessage.txt
+        switch(currentChar)
+        {
+            case('\n'):
+                decipheredMessage << endl;
+                cout << "Outputting new line" << endl;
+                break;
+            case('~'):
+                decipheredMessage << " ";
+                cout << "Outputting space" << endl;
+                break;
+            default:
+                currentChar++;
+                cout << "Outputting shifted character: " << currentChar << endl;
+                decipheredMessage << currentChar;
+        }
     }
 
-    myCatsAgesIn.close(); // close the file
+    secretMessage.close();
+    decipheredMessage.close();
     return 0;
 }
