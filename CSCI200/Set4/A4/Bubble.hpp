839010c9a2f6fa45af7b4732d303bc69909a20f6
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-04-09 20:08:40 -0600

    s;ldfjhg;lsdjhfg

--- CSCI200/Set4/A4/Bubble.hpp
+++ CSCI200/Set4/A4/Bubble.hpp
@@ -8,54 +8,93 @@ using namespace std;
 class Bubble
 {
 private:
-    CircleShape bubble;
-    double xDir;
-    double yDir;
+    CircleShape _bubble;
+    double _xDir;
+    double _yDir;
+    const double _defaultXDir = 1;
+    const double _defaultYDir = 2;
+    const int _defaultRadius = 50;
+    const Color _defaultColor = Color::White;
 public:
     Bubble();
-    Bubble(double, double);
+    Bubble(int x, int y);
+    Bubble(int x, int y, double xDir, double yDir, int rad, Color color);
+    void updatePosition(int winHeight, int winWidth);
+    void draw(RenderWindow&);
+
+    // Getters
     double getXDir();
     double getYDir();
-    void setXDir(double);
-    void setYDir(double);
-    void draw(RenderWindow&);
+    int getX();
+    int getY();
+
+    // Setters
+    void setXDir(double x);
+    void setYDir(double y);
+    void setX(int x);
+    void setY(int y);
 };
+
+/* ---------- Method Definitions ---------- */
 
 Bubble::Bubble()
 {
-    bubble.setPosition(100, 100);
-    bubble.setFillColor(Color::White);
-    bubble.setRadius(100);
-    xDir = 0;
-    yDir = 0;
+    _bubble.setPosition(100, 100);
+    _bubble.setRadius(_defaultRadius);
+    _bubble.setFillColor(_defaultColor);
+    _xDir = _defaultXDir;
+    _yDir = _defaultYDir;
 }
 
-Bubble::Bubble(double x, double y)
+Bubble::Bubble(int x, int y)
 {
-    bubble.setPosition(100, 100);
-    bubble.setRadius(100);
-    bubble.setFillColor(Color::White);
-    xDir = x;
-    yDir = y;
+    _bubble.setPosition(x, y);
+    _bubble.setRadius(_defaultRadius);
+    _bubble.setFillColor(_defaultColor);
+    _xDir = _defaultXDir;
+    _yDir = _defaultYDir;
 }
 
-inline double Bubble::getXDir() {return xDir;}
-
-inline double Bubble::getYDir() {return yDir;}
-
-inline void Bubble::setXDir(double x) 
+Bubble::Bubble(int x, int y, double xDir, double yDir, int rad, Color color)
 {
-    if(x>0) xDir = x;
+    _bubble.setPosition(x, y);
+    _bubble.setRadius(rad);
+    _bubble.setFillColor(color);
+    _xDir = xDir;
+    _yDir = yDir;
 }
 
-inline void Bubble::setYDir(double y)
+void Bubble::updatePosition(int winHeight, int winWidth)
 {
-    if(y>0) yDir = y;
+    if(winHeight) _xDir = _xDir + 1 - 1;
+    if(((_bubble.getPosition().x + _bubble.getRadius()*2) >= winWidth) || // If the right edge of the bubble is greater than the right edge of the window
+        (_bubble.getPosition().x <= 0))                                   // If the left edge of the bubble is less than the left edge of the window
+    {
+        _xDir = -_xDir;
+    }
+    if(((_bubble.getPosition().y + _bubble.getRadius()*2) >= winHeight) || // Check bottom edge of bubble is in bounds
+         _bubble.getPosition().y <= 0)                                     // Check if bubble hits top
+    {
+        _yDir = -_yDir;
+    }
+    _bubble.move(_xDir, _yDir);
 }
 
 void Bubble::draw(RenderWindow& window)
 {
-    window.draw(bubble);
+    window.draw(_bubble);
 }
+
+// Getters
+inline double Bubble::getXDir() { return _xDir; }
+inline double Bubble::getYDir() { return _yDir; }
+inline int Bubble::getX() { return _bubble.getPosition().x; }
+inline int Bubble::getY() { return _bubble.getPosition().y; }
+
+// Setters
+inline void Bubble::setXDir(double x) { _xDir = x; }
+inline void Bubble::setYDir(double y) { _yDir = y; }
+inline void Bubble::setX(int x) { _bubble.setPosition( x, _bubble.getPosition().y ); }
+inline void Bubble::setY(int y) { _bubble.setPosition( _bubble.getPosition().x, y ); }
 
 #endif//BUBBLE_HPP
--- CSCI200/Set4/A4/main.cpp
+++ CSCI200/Set4/A4/main.cpp
@@ -4,11 +4,18 @@ using namespace sf;
 #include <iostream>
 using namespace std;
 
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+
 #include "./Bubble.hpp"
+
 
 int main() {
     // create a window
-    RenderWindow window( VideoMode(640, 640), "SFML Test" );
+    const int winWidth = 640;
+    const int winHeight = 640;
+    RenderWindow window( VideoMode(winWidth, winHeight), "SFML Test" );
 
     /////////////////////////////////////
     // BEGIN ANY FILE LOADING
@@ -18,7 +25,38 @@ int main() {
     //  END  ANY FILE LOADING
     /////////////////////////////////////
 
-    Bubble bubble;
+    srand(time(NULL));
+
+    // Create the bubble object
+    const int startingBubbles = 5;
+    const int minPos = 100;
+    const int maxPos = 400;
+    const double minDir = -4;
+    const double maxDir = 4;
+    const int minRad = 10;
+    const int maxRad = 50;
+    vector<Bubble> bubbles;
+    for(int i=0; i<startingBubbles; i++)
+    {
+        // Generate a random bubble
+        bubbles.push_back(
+            Bubble(
+                rand() % (maxPos-minPos) + minPos,                      // x
+                rand() % (maxPos-minPos) + minPos,                      // y
+                ((double)rand() / RAND_MAX) * (maxDir-minDir) + minDir, // xDir
+                ((double)rand() / RAND_MAX) * (maxDir-minDir) + minDir, // yDir
+                rand() % (maxRad-minRad) + minRad,                      // Radius
+                Color(                                                  // Color
+                    rand() % 256, // R
+                    rand() % 256, // G
+                    rand() % 256  // B
+                )
+            )
+        );
+    }
+
+    // For tracking elapsed time
+    Clock clock;
 
     // create an event object once to store future events
     Event event;
@@ -31,7 +69,14 @@ int main() {
         /////////////////////////////////////
         // BEGIN DRAWING HERE
 
-        bubble.draw(window);
+
+
+        for(size_t i=0; i<bubbles.size(); i++)
+        {
+            bubbles.at(i).draw(window);
+        }
+
+
 
         //  END  DRAWING HERE
         /////////////////////////////////////
@@ -49,10 +94,54 @@ int main() {
                 // tell the window to close
                 window.close();
             }
+
             // check addition event types to handle additional events
+            if (event.type == Event::MouseButtonPressed && event.mouseButton.button == Mouse::Left) {
+                
+                int tempRadius = rand() % (maxRad-minRad) + minRad;
+                bubbles.push_back(
+                    Bubble(
+                        event.mouseButton.x - tempRadius,                       // x = where user clicked
+                        event.mouseButton.y - tempRadius,                       // y = where user clicked
+                        ((double)rand() / RAND_MAX) * (maxDir-minDir) + minDir, // xDir
+                        ((double)rand() / RAND_MAX) * (maxDir-minDir) + minDir, // yDir
+                        tempRadius,                                             // Radius
+                        Color(                                                  // Color
+                            rand() % 256, // R
+                            rand() % 256, // G
+                            rand() % 256  // B
+                        )
+                    )
+                );
+            }
+
+            if (event.type == sf::Event::KeyPressed) {
+                // D removes last bubble
+                if (event.key.code == sf::Keyboard::D) {
+                    if(bubbles.size() > 0) bubbles.pop_back();
+                }
+
+                // Escape and Q close the window
+                if (event.key.code == sf::Keyboard::Escape || event.key.code == sf::Keyboard::Q) {
+                    window.close();
+                }
+            }
         }
         //  END  EVENT HANDLING HERE
         /////////////////////////////////////
+
+        // Every 1/60th of a second, update the bubbles
+        Time elapsedTime = clock.getElapsedTime();
+        if(elapsedTime.asSeconds() >= 1.0/60) {
+
+            for(size_t i=0; i<bubbles.size(); i++)
+            {
+                bubbles.at(i).updatePosition(winWidth, winHeight);
+            }
+
+            clock.restart();
+        }
+
     }
 
     return 0;
