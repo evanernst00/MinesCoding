f0d69788655a9dc7ac42bd2c72bf791139dafb0e
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-04-15 20:48:06 -0600

    Started A5

---
+++ CSCI200/Set5/A5/main.cpp
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.hpp>
+using namespace sf;
+
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    RenderWindow window( VideoMode(640, 640), "Polygon Land" );
+
+    Event event;
+
+
+    while( window.isOpen() ) 
+    {
+        window.clear();
+
+        /////////////////////////////////////
+        // BEGIN DRAWING HERE
+
+
+
+        //  END  DRAWING HERE
+        /////////////////////////////////////
+
+
+
+        window.display();
+
+        while( window.pollEvent(event) ) 
+        {
+            if(event.type == Event::Closed) 
+            {
+
+                window.close();
+            }
+        }
+    }
+
+    return 0;
+}
