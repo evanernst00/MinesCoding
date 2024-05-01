e7c2eabc22c4ca8b3b64de19aa0c5d2eedd903b8
Evan Ernst <50885631+evanernst00@users.noreply.github.com>
2024-04-30 19:39:06 -0600

    Removed non code files

--- CSCI200_Old/CSCI200_COURSE_MATERIALS/Files/Style Guide and Examples/Functions.cpp
+++
@@ -1,9 +0,0 @@
-#include "Functions.h"                     // include the file with the corresponding prototypes
-
-int calculator_add( int x, int y ) {       // function definition
-  return x + y;
-}
-
-int calculator_sub( int x, int y ) {
-  return x - y;
-}
--- CSCI200_Old/misc/lectures/Lec08/add_functions.cpp
+++
@@ -1,6 +0,0 @@
-#include "add_function.h"
-
-int add(int x, int y)
-{
-    return x + y;
-}
